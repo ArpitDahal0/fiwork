46a4a9dd91bfc158cb29d857b8d095d38ff8c77e
ArpitDahal0 <tothearpit@gmail.com>
2026-01-15 09:09:33 +0545

    added notifications

---
+++ Client/src/pages/Notifications.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Notifications() {
+  return (
+    <div className='bg-secondary min-h-screen'>Here goes notification</div>
+  )
+}
+
+export default Notifications
