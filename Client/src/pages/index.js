7325104f204aaba845232e1922872be6abbd609f
ArpitDahal0 <tothearpit@gmail.com>
2026-01-19 08:15:21 +0545

    added index

---
+++ Client/src/pages/index.js
@@ -0,0 +1,45 @@
+import Home from "./Home";
+import Login from "./Login";
+import Signup from "./Signup";
+import Logout from "./Logout";
+import Dashboard from "./Dashboard";
+import Profile from "./Profile";
+import Notifications from "./Notifications";
+import Settings from "./Settings";
+import VerifyEmail from "./VerifyEmail";
+import Kyc from "./Kyc";
+import SwitchRole from "./SwitchRole";
+import Jobs from "./Jobs";
+import Inbox from "./Inbox";
+import Account from "./Account";
+import Followers from "./Followers";
+import Following from "./Following";
+import Pay from "./Pay";
+import PayFailure from "./PayFailure";
+import PaySuccess from "./PaySuccess";
+import SingleTransaction from "./SingleTransaction";
+import Transactions from "./Transactions";
+
+export {
+    Home,
+    Login,
+    Signup,
+    Logout,
+    Dashboard,
+    Profile,
+    Notifications,
+    Settings,
+    VerifyEmail,
+    Kyc,
+    SwitchRole,
+    Jobs,
+    Inbox,
+    Account,
+    Followers,
+    Following,
+    Pay,
+    PayFailure,
+    PaySuccess,
+    SingleTransaction,
+    Transactions,
+};
