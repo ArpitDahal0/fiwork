cc338836c9da6d95d17e931925acc8398032d11d
ArpitDahal0 <tothearpit@gmail.com>
2026-02-10 14:17:44 +0545

    added index postedjobs settings

---
+++ Client/src/stores/index.js
@@ -0,0 +1,5 @@
+import { useAuth } from "./Auth";
+import { useChat } from "./Chat";
+import { usePostedJobs } from "./PostedJobs";
+import { useSetting } from "./Setting";
+export { useAuth, usePostedJobs, useSetting, useChat };
